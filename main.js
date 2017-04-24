e54f0b37ae404320bc6c21814645a84c4ff6e048
Hafiz Mughees <mughees605@gmail.com>
2017-04-24 01:15:26 -0700

    28 to 34

---
+++ main.js
@@ -0,0 +1,54 @@
+// var currentAge = prompt("Enter your age");
+// var sub = currentAge - "33";
+// console.log(sub);
+
+// var currentAge = prompt("Enter your age");
+// var age = parseInt(currentAge);
+// console.log(age);
+
+// var currentAge = prompt("Enter your age");
+// var age = parseFloat(currentAge);
+// console.log(age);
+
+//////////////////////////////////////////////////// CHAP 29///////////////////////////////////////
+// var integerString = "24"
+// var num = Number(integerString);
+// console.log(num);
+
+
+// var floatingNumString = "24.9876";
+// var num = Number(floatingNumString);
+// console.log(num);
+
+
+////////////////////////////////////////////////// CHAP 31 ////////////////////////////////////////
+
+// var rightNow = new Date();
+// var theDay = rightNow.getDay();
+// console.log(theDay);
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+
+////////////////////////////////////////////////// CHAP 32 ///////////////////////////////////////
+
+
+// var d = new Date();
+// var currentMonth = d.getMonth();
+// var Time - d.getTime();
+//
+
+//  console.log(Time);
+
+/////////////////////////////////////////////////// CHAP 33 ///////////////////////////////////////
+
+// var Today = new Date();
+// var DoomsDay = new Date("2017 ,april ,28")
+// var TodayTime = Today.getTime();
+// var DoomsTime = DoomsDay.getTime();
+// var msDiff = DoomsTime - TodayTime;
+// console.log(Math.floor(msDiff/ (1000 * 60 * 60 * 24)));
+
+
